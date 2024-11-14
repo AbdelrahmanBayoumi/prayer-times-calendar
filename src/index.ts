a3663cd4a94e6d3c1738d039cf096f2c3f50f641
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2024-11-14 16:53:31 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { CalculationMethod, Coordinates } from 'adhan';
 import { generatePrayerCalendar } from './utils/calendar-utils';
 
-// Example dynamic inputs (these can be taken from user input or environment variables)
 const coordinates = new Coordinates(31.1981, 29.9192); // Alexandria, Egypt
 const calculationMethod = CalculationMethod.Egyptian();
 const startDate = process.env.START_DATE || '2024-11-14';
